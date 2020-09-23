e71e0856f3697aef0d53b518ac846ece2e0474ea
Carter Snook <cartersnook04@gmail.com>
2020-09-23 16:44:14 -0500

    Added TweetWithheld.scope

--- models/tweet/withheld.ts
+++ models/tweet/withheld.ts
@@ -1,4 +1,5 @@
 export interface TweetWithheld {
   copyright: boolean;
   country_codes: string[]; // Should check to see if country code is one of twitter's valid country codes
+  scope?: "tweet" | "user";
 }
